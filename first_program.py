2162cd15afacad2c7eaa6127dae5c0fb94fca34a
aanchalcoder1 <mailtoaanchalyadav@gmail.com>
2025-08-21 11:58:29 +0530

    First upload

---
+++ first_program.py
@@ -0,0 +1,44 @@
+# age=int(input("enter a number"))
+# if (age>=19):
+#     if (age>=60):
+#         print("cannot drive")
+#     else:
+#         print("can drive")
+# else:
+#     print("cannot drive")
+
+
+# marks=int(input("enter marks of students:"))
+# if(marks>=90):
+#     grade="A" 
+# elif(marks>=80 and marks<90):
+#     grade="B"
+# elif(marks>=70 and marks<80):
+#     grade="C"
+# print("grade of the students->",grade)
+
+# age=34
+# if(age>=18):
+#     if (age>=80):
+#         print("cannot drive")
+#     else:
+#         print("can drive")
+# else:
+#     print("cannot drive")
+  
+
+# 
+# marks=[95.74,93.56,86.43,56.95]
+# print(marks[1:3])
+# 
+# movies=[]
+# mov1=input("enter 1st movie:")
+# mov2=input("enter 2nd movie:")
+
+# collection= {1,2,2,2,"hellow", "world" ,"world" ,4}
+# print(collection)
+# print(len(collection))
+
+collection= {1,2,2,2,"hellow", "world" ,"world" ,4}
+print(collection.pop())
+print(collection.pop())
